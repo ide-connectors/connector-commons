b46be899c4e4e60e6efdcdff02ca1b252f8a05ae
Janusz Gorycki <jgorycki@atlassian.com>
2009-01-20 14:07:05 +0000

    PL-943 - Self-signed certificate dialog comes up every time I start IDEA, even after I press the "Accept" button

--- com.atlassian.connector.commons/src/main/java/com/atlassian/theplugin/commons/configuration/PluginConfigurationBean.java
+++ com.atlassian.connector.commons/src/main/java/com/atlassian/theplugin/commons/configuration/PluginConfigurationBean.java
@@ -1,23 +1,12 @@
-/**
- * Copyright (C) 2008 Atlassian
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *    http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.atlassian.theplugin.commons.configuration;
 
 import com.atlassian.theplugin.commons.util.HttpConfigurableAdapter;
 
+/**
+ * User: jgorycki
+ * Date: Jan 20, 2009
+ * Time: 2:48:30 PM
+ */
 public class PluginConfigurationBean implements PluginConfiguration {
     private BambooConfigurationBean bambooConfiguration = new BambooConfigurationBean();
 
@@ -202,3 +191,4 @@ public class PluginConfigurationBean implements PluginConfiguration {
         return result;
     }
 }
+

23db82756adf92ab972bf89e9d7c815dae214051
Piotr Maruszak <pmaruszak@atlassian.com>
2009-10-07 10:00:24 +0000

    Moving JIRA client side to commons

--- com.atlassian.connector.commons/src/main/java/com/atlassian/connector/commons/jira/cache/CacheConstants.java
+++ com.atlassian.connector.commons/src/main/java/com/atlassian/connector/commons/jira/cache/CacheConstants.java
@@ -4,7 +4,7 @@ package com.atlassian.connector.commons.jira.cache;
  * @author pmaruszak
  * @date Oct 7, 2009
  */
-public class CacheConstants {
+public final class CacheConstants {
     private CacheConstants() {
     }
     

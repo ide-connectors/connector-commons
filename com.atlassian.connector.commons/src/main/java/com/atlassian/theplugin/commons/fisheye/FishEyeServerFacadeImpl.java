0d28d337d5b831d19ad47772c579189cc03157ae
Pawel Niewiadomski <pniewiadomski@atlassian.com>
2009-10-21 14:10:54 +0000

    PLE-510

--- com.atlassian.connector.commons/src/main/java/com/atlassian/theplugin/commons/fisheye/FishEyeServerFacadeImpl.java
+++ com.atlassian.connector.commons/src/main/java/com/atlassian/theplugin/commons/fisheye/FishEyeServerFacadeImpl.java
@@ -9,7 +9,6 @@ import com.atlassian.theplugin.commons.fisheye.api.rest.FishEyeRestSession;
 import com.atlassian.theplugin.commons.remoteapi.RemoteApiException;
 import com.atlassian.theplugin.commons.remoteapi.RemoteApiMalformedUrlException;
 import com.atlassian.theplugin.commons.remoteapi.rest.HttpSessionCallback;
-
 import java.util.Collection;
 
 /**
@@ -24,20 +23,20 @@ public class FishEyeServerFacadeImpl implements FishEyeServerFacade2 {
 	}
 
 	public void testServerConnection(HttpConnectionCfg serverCfg) throws RemoteApiException {
-		 testServerConnection(serverCfg.toConnectionCfg());
+		testServerConnection(serverCfg.toConnectionCfg());
 	}
 
-    public void testServerConnection(ConnectionCfg serverCfg) throws RemoteApiException {
-       FishEyeSession fishEyeSession = getSession(serverCfg);
+	public void testServerConnection(ConnectionCfg serverCfg) throws RemoteApiException {
+		FishEyeSession fishEyeSession = getSession(serverCfg);
 		fishEyeSession.login(serverCfg.getUsername(), serverCfg.getPassword().toCharArray());
 
 		// well, we need to call _something_ to see if it worked, in case of anonymous access
 		fishEyeSession.getRepositories();
 
 		fishEyeSession.logout();
-    }
+	}
 
-    public ServerType getServerType() {
+	public ServerType getServerType() {
 		return ServerType.FISHEYE_SERVER;
 	}
 

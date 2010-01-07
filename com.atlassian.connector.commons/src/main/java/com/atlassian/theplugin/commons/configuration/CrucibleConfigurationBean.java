f2422e5a87a0c6e27b0e1bb63a16648f147aee58
Piotr Maruszak <pmaruszak@atlassian.com>
2010-01-07 10:01:58 +0000

    cs fixes

--- com.atlassian.connector.commons/src/main/java/com/atlassian/theplugin/commons/configuration/CrucibleConfigurationBean.java
+++ com.atlassian.connector.commons/src/main/java/com/atlassian/theplugin/commons/configuration/CrucibleConfigurationBean.java
@@ -26,8 +26,8 @@ public class CrucibleConfigurationBean {
     private static final int MIN_REVIEW_CREATION_TIMEOUT = 1;
     private int reviewCreationTimeout = DEFAULT_REVIEW_CREATION_TIMEOUT;
     public static final int DEFAULT_REVIEW_FILE_CACHE_SIZE = 100;
-    public static int MIN_REVIEW_FILE_CACHE_SIZE = 50;
-    public static int MAX_REVIEW_FILE_CACHE_SIZE = 1000;
+    public static final int MIN_REVIEW_FILE_CACHE_SIZE = 50;
+    public static final int MAX_REVIEW_FILE_CACHE_SIZE = 1000;
     private int reviewFileCacheSize = DEFAULT_REVIEW_FILE_CACHE_SIZE;
 
     public CrucibleConfigurationBean() {

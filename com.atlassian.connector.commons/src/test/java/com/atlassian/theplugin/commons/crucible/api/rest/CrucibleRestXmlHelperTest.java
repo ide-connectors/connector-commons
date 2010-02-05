d1bbc7f672cb4233e50497342cc2af45a8c0c79d
Piotr Stefaniak <pstefaniak@atlassian.com>
2010-02-05 09:11:23 +0000

    PL-1611: Clean up warnings when rebuilding project in IDEA unit tests for versioned comments line ranges - this is preparation before removing deprecations in versioned comments

--- com.atlassian.connector.commons/src/test/java/com/atlassian/theplugin/commons/crucible/api/rest/CrucibleRestXmlHelperTest.java
+++ com.atlassian.connector.commons/src/test/java/com/atlassian/theplugin/commons/crucible/api/rest/CrucibleRestXmlHelperTest.java
@@ -1,6 +1,11 @@
 package com.atlassian.theplugin.commons.crucible.api.rest;
 
+import com.atlassian.connector.commons.misc.IntRange;
+import com.atlassian.connector.commons.misc.IntRanges;
+import com.atlassian.theplugin.commons.crucible.api.model.CrucibleFileInfo;
 import com.atlassian.theplugin.commons.crucible.api.model.CrucibleProject;
+import com.atlassian.theplugin.commons.crucible.api.model.Review;
+import com.atlassian.theplugin.commons.crucible.api.model.VersionedComment;
 import com.atlassian.theplugin.crucible.api.rest.cruciblemock.CrucibleMockUtil;
 import junit.framework.TestCase;
 import org.jdom.Document;
@@ -10,6 +15,8 @@ import org.jdom.input.SAXBuilder;
 import org.jdom.xpath.XPath;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -35,5 +42,67 @@ public class CrucibleRestXmlHelperTest extends TestCase {
 
         assertTrue(cp != null);
         assertEquals(5, cp.getAllowedReviewers().size());
-    }
+
+
+		doc = builder.build(new CrucibleMockUtil().getResource("reviewDetailsResponse-testLineRanges.xml"));
+		xpath = XPath.newInstance("detailedReviewData");
+		elements = xpath.selectNodes(doc);
+
+		Review review = null;
+		try {
+			review = CrucibleRestXmlHelper.parseFullReview("http://localhost", "pstefaniak", elements.get(0), false);
+		} catch (ParseException e) {
+			fail(); // check "reviewDetailsResponse-testLineRanges.xml" - should be valid .xml...
+		}
+		assertTrue(review != null);
+
+		Iterator<CrucibleFileInfo> it = review.getFiles().iterator();
+		List<VersionedComment> versionedComments = it.next().getVersionedComments();
+
+		VersionedComment comment = versionedComments.get(0);
+		assertEquals(comment.getMessage(), "this is yellow coment");
+		assertEquals(comment.getFromStartLine(), 0);
+		assertEquals(comment.getFromEndLine(), 0);
+		assertEquals(comment.getToStartLine(), 0);
+		assertEquals(comment.getToEndLine(), 0);
+		assertNull(comment.getToLineRanges());
+		assertNull(comment.getFromLineRanges());
+		assertNull(comment.getLineRanges());
+
+		comment = versionedComments.get(1);
+		assertEquals(comment.getMessage(), "this is green comment");
+		assertEquals(comment.getFromStartLine(), 0);
+		assertEquals(comment.getFromEndLine(), 0);
+		assertEquals(comment.getToStartLine(), 52);
+		assertEquals(comment.getToEndLine(), 52);
+		assertEquals(comment.getToLineRanges(), new IntRanges(new IntRange(52)));
+		assertNull(comment.getFromLineRanges());
+		assertEquals(comment.getLineRanges().get("51224"), new IntRanges(new IntRange(52)));
+
+		comment = versionedComments.get(2);
+		assertEquals(comment.getMessage(), "this is red coment");
+		assertEquals(comment.getFromStartLine(), 0);
+		assertEquals(comment.getFromEndLine(), 0);
+		assertEquals(comment.getToStartLine(), 51);
+		assertEquals(comment.getToEndLine(), 51);
+		assertEquals(comment.getToLineRanges(), new IntRanges(new IntRange(51)));
+		assertNull(comment.getFromLineRanges());
+		assertEquals(comment.getLineRanges().get("38347"), new IntRanges(new IntRange(51)));
+
+		comment = versionedComments.get(3);
+		assertEquals(comment.getMessage(), "this is blue comment (lines 49, 52, 53)");
+		assertEquals(comment.getFromStartLine(), 49);
+		assertEquals(comment.getFromEndLine(), 53);
+		assertEquals(comment.getToStartLine(), 49);
+		assertEquals(comment.getToEndLine(), 54);
+		assertEquals(comment.getToLineRanges(), new IntRanges(new IntRange(49), new IntRange(53,54)));
+		assertEquals(comment.getFromLineRanges(), new IntRanges(new IntRange(49), new IntRange(52,53)));
+		assertEquals(comment.getLineRanges().get("38347"), new IntRanges(new IntRange(49), new IntRange(52,53)));
+		assertEquals(comment.getLineRanges().get("51224"), new IntRanges(new IntRange(49), new IntRange(53,54)));
+
+
+
+
+
+	}
 }

a790db367bd624f6e012008e6755b4d20874feaf
Jacek Jaroczynski <jjaroczynski@atlassian.com>
2010-10-13 13:31:24 +0200

    new Bamboo API support (test fixed)

--- com.atlassian.connector.commons/src/test/java/com/atlassian/theplugin/commons/bamboo/api/BambooSessionTest.java
+++ com.atlassian.connector.commons/src/test/java/com/atlassian/theplugin/commons/bamboo/api/BambooSessionTest.java
@@ -909,6 +909,7 @@ public class BambooSessionTest extends AbstractSessionTest {
 		final String charset1 = "UTF-8";
 		final String charset2 = "UTF-16";
 		mockServer.expect("/api/rest/login.action", new LoginCallback(USER_NAME, PASSWORD));
+		mockServer.expect("/api/rest/getBambooBuildNumber.action", new BamboBuildNumberCalback());
 		mockServer.expect("/download/myplan/build_logs/myplan-123.log", new BuildLogCallback(TEXT, charset1));
 		mockServer.expect("/download/myplan/build_logs/myplan-123.log", new BuildLogCallback(TEXT, charset2));
 		mockServer.expect("/api/rest/logout.action", new LogoutCallback());

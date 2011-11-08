cc0fccc60abe118109cd4f5f4285cb08dbb0102b
Piotr Maruszak <pmaruszak@atlassian.com>
2011-11-08 13:32:18 +0100

    PL-2552

--- com.atlassian.connector.commons/src/main/java/com/atlassian/theplugin/commons/cfg/xstream/HomeDirSharedConfigurationImpl.java
+++ com.atlassian.connector.commons/src/main/java/com/atlassian/theplugin/commons/cfg/xstream/HomeDirSharedConfigurationImpl.java
@@ -66,14 +66,14 @@ public class HomeDirSharedConfigurationImpl
 			   throw new ServerCfgFactoryException("Cannot lock shared file: " + outputFile.getAbsolutePath());
 			}
 			SharedServerList storedList = load();
-			if (outputFile.exists() && outputFile.canWrite() && serversInfo.size() > 0) {
-				if (storedList != null) {
+			if (outputFile.exists() && outputFile.canWrite()) {
+//				if (storedList != null && serversInfo.size() > 0) {
 					//merge existing cfg
-					document = createJDom(SharedServerList.merge(serversInfo, storedList));
-                    //document = createJDom(serversInfo);
-				} else {
+					//document = createJDom(SharedServerList.merge(serversInfo, storedList));
+//                    document = createJDom(serversInfo);
+//				} else {
 					document = createJDom(serversInfo);
-				}
+//				}
 
 				writeXmlFile(document.getRootElement(), outputFile);
 			} else {
